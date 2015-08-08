1ece783063fb98a4f9f9698fd7ab3cef00f2c69c
Chaofan Tan <tcf245@gmail.com>
2015-08-08 17:05:47 +0800

    2048game
    15-8-8

---
+++ src/main/java/com/cn/MainActivity.java
@@ -0,0 +1,66 @@
+package com.cn;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView tvScore;
+    private int score = 0;
+    private static  MainActivity mainActivity = null;
+
+    public MainActivity(){
+        mainActivity = this;
+    }
+    public static MainActivity getInstance(){
+        return mainActivity;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvScore = (TextView) findViewById(R.id.tvScore);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void clearScore(){
+        score = 0;
+        showScore();
+    }
+
+    public void showScore(){
+        tvScore.setText(score + "");
+    }
+
+    public void addScore(int s){
+        score+= s;
+        showScore();
+    }
+
+}
